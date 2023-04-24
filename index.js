41fafd32ae757f41cb91da7d4fa827f8bf134de3
Samuel Drinkwater <samueldrinkwater@gmail.com>
2023-04-25 09:16:11 +1000

    Initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const fs = require("fs");
+const csv = require("csv-parser");
+
+// CSV file path
+const csvFilePath = "./data/legacy-verified.csv";
+
+// Directory in which to create symlinks
+const symlinkDir = "./syms/";
+
+// File to target
+const targetPath = `./assets/legacy-verified.svg`;
+
+// Read the CSV file
+fs.createReadStream(csvFilePath)
+  .pipe(csv())
+  .on("data", (user) => {
+    // Create the symlink
+    fs.symlink(targetPath, symlinkDir + user.id, (err) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      console.log(`${user.id} created successfully`);
+    });
+  })
+  .on("end", () => {
+    console.log("CSV file successfully processed");
+  });
