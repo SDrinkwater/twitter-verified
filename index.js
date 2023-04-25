ec24377ab6048130be36fb316e8ffbf6fbb44f19
Samuel Drinkwater <samueldrinkwater@gmail.com>
2023-04-25 11:52:44 +1000

    Test with 1k ids

--- index.js
+++ index.js
@@ -5,15 +5,22 @@ const csv = require("csv-parser");
 const csvFilePath = "../data/legacy-verified.csv";
 
 // Directory in which to create symlinks
-const symlinkDir = "./";
+const symlinkDir = ".";
 
 // File to target
 const targetPath = `../assets/legacy-verified.svg`;
+
+const writeLimit = 1000;
+let writeCount = 0;
 
 // Read the CSV file
 fs.createReadStream(csvFilePath)
   .pipe(csv())
   .on("data", (user) => {
+    writeCount++;
+    if (writeCount > writeLimit) {
+      return;
+    }
     // Create the symlink
     fs.symlink(targetPath, `${symlinkDir}/${user.id}`, (err) => {
       if (err) {
